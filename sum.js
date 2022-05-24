60f76dfbf70dc6a19c2c1b9a1eeb3e26c465bf42
sudhir <sudhir@workplacefabric.com>
2022-05-24 09:43:05 +0530

    The method can take up to two numbers

--- sum.js
+++ sum.js
@@ -5,6 +5,9 @@ function sum(string) {
 	if (string.indexOf('-') >= 0) {
 		throw new Error('negatives not allowed');
 	}
+	if (string.split(',').length > 2) {
+		return "The method can take up to two numbers"
+	}
 	return string.split(',').reduce((a, b) => a + +b, 0);
 }
 module.exports = sum;
--- sum.test.js
+++ sum.test.js
@@ -12,3 +12,6 @@ test('string "1" it will return 1', () => {
 test('a negative number will throw an exception "negatives not allowed"', () => {
 	expect(() => { sum("-1,2") }).toThrow('negatives not allowed');
 })
+test('The method can take up to two numbers', () => {
+	expect(sum("1,2,3")).toBe('The method can take up to two numbers');
+});
