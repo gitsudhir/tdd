68cd96c7bb746fe5f44cc958e6375ede6c3943e2
sudhir <sudhir@workplacefabric.com>
2022-05-24 09:11:36 +0530

    string "1,2" it will return 3

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,6 @@ const sum = require('./sum');
 test('an empty string it will return 0', () => {
 	expect(sum("")).toBe(0);
 });
+test('string "1,2" it will return 3', () => {
+	expect(sum("1,2")).toBe(3);
+});
